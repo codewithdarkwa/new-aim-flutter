e009d3becc10fb86e755a8d0547bf70312844607
codewithdarkwa <jnrdrkw1@gmail.com>
2022-10-04 18:15:07 +0000

    working on the home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,6 +11,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(70),
@@ -66,7 +67,101 @@ class _HomeState extends State<Home> {
                       ],
                     ),
                   ),
-                  const Text('Hi')
+                  const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'Your next class',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  const ClassSchedule(),
+                  const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'What would you like to do ?',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: const Color(0xfff3e9e9),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Icon(Icons.check),
+                                Text(
+                                  'Check Results',
+                                  style: TextStyle(fontSize: 19),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: const Color(0xfff3e9e9),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Icon(
+                                    Icons.wallet,
+                                  ),
+                                ),
+                                Text(
+                                  'Pay   Fees',
+                                  style: TextStyle(fontSize: 19),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: const Color(0xfff3e9e9),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Icon(Icons.check),
+                                Text(
+                                  'Register Courses',
+                                  style: TextStyle(fontSize: 19),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -78,7 +173,6 @@ class _HomeState extends State<Home> {
   }
 }
 
-// ignore: non_constant_identifier_names
 Widget CustomAppBar() {
   return AppBar(
     title: const Text('Home'),
@@ -87,3 +181,34 @@ Widget CustomAppBar() {
     elevation: 0,
   );
 }
+
+class ClassSchedule extends StatelessWidget {
+  const ClassSchedule({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
+      child: Container(
+        height: 200,
+        decoration: BoxDecoration(
+          color: const Color(0xfff3e9e9),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: const Center(
+          child: Text(
+            'Your next class will appear here.',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: non_constant_identifier_names
+
