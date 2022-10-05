20f618929c59e3bc636d120d534519d84d855581
codewithdarkwa <jnrdrkw1@gmail.com>
2022-10-05 19:02:07 +0000

    done with the download page

--- lib/pages/download.dart
+++ lib/pages/download.dart
@@ -29,6 +29,54 @@ class Download extends StatelessWidget {
                   ),
                   color: Colors.white,
                 ),
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            width: 120,
+                            decoration: BoxDecoration(
+                                color: const Color(0xff6b0b02),
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Row(
+                              children: const [
+                                Icon(
+                                  Icons.delete,
+                                  color: Colors.white,
+                                  size: 24,
+                                ),
+                                Text(
+                                  'Clear All',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Divider(),
+                        ],
+                      ),
+                      Container(
+                        width: 500,
+                        height: 130,
+                        decoration: BoxDecoration(
+                          color: const Color(0xfff3e9e9),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'You have no downloads.',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
